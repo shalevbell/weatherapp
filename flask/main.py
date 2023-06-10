c359772bf3c3d9d96ecc0c58aa53ee0f97a689e9
root <root@ip-172-31-4-246.eu-central-1.compute.internal>
2023-06-10 14:02:13 +0000

    adding flask/ directory

---
+++ flask/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request  # needed to use flask.
+from app import *  # imports all functions from app.py.
+import json
+from datetime import date
+from os import listdir
+from os.path import isfile, join
+import os
+
+weather_app = Flask(__name__)  # create a new application instance called weather_app.
+
+
+@weather_app.route('/', methods=('GET', 'POST'))  # Sets an app rout and gives it GET & POST methods.
+def search():
+    if request.method == "POST":  # checks if the user is sending a POST request.
+        if request.form['submit_button'] == 'history':  # If user presses history button then execute
+            files = [f for f in listdir("static/history/") if isfile(join("static/history/", f))]
+            return render_template('weather_history.html', history=files)
+        elif request.form['submit_button'] == 'check':  # If user presses search button then execute
+            try:  # Checks if location_to_dict raised a typeError because the location was not valid.
+                location = request.form["input_location"]
+                location_dict = location_to_dict(location)
+                with open(f"static/history/{location}:{date.today()}.json", 'w') as outfile:
+                    json.dump(location_dict, outfile, indent=4)
+            except TypeError:  # send user to Error page.
+                return render_template('weather_error.html')
+
+        return render_template('weather_main.html', location_dict=location_dict )
+
+    return render_template('weather_search.html')
+
+
+if __name__ == "__main__":
+    weather_app.run(host='0.0.0.0')  # Runs weather_app.
